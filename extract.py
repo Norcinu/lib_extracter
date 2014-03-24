25c9d674d28d8fa60378a05b6eab7b4176c50495
Steven Gleed <ledbybyson@gmail.com>
2014-03-24 23:58:40 +0000

    Initial commit for work tomorrow

---
+++ extract.py
@@ -0,0 +1,25 @@
+
+
+bo_header = "C:\heber\projects\1224Source\bo.h"
+cs_native = "C:\output\Native.cs"
+
+
+def parse_bo_lib():
+    with open(bo_header, 'r') as content_file:
+        content = content_file.read()
+
+
+def write_cs_native():
+
+
+def write_cpp_source():
+    
+
+def write_cpp_header():
+
+
+
+
+
+
+
