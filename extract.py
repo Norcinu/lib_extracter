6d3e4c98ff2289f025530b4f5521601ef32cbf6b
Steven Gleed <ledbybyson@gmail.com>
2014-04-01 14:17:20 +0100

    Basic Framework - off to do other work

--- extract.py
+++ extract.py
@@ -1,10 +1,11 @@
 import os
-import re
 
 bo_header = os.getcwd() + "/bo.h"
 cs_native = os.getcwd() + "/output/Native.cs"
 
-print bo_header;
+prefix = "Bo_"
+outputs = ['BoLibGetValues', 'BoLibSetValues', 'BoLibGeneral']
+extensions = ['.cpp', '.h']
 
 sets_array = []
 gets_array = []
@@ -15,17 +16,36 @@ def parse_bo_lib():
         for line in content_file:
             if line != "\n":
                 if "Add" in line or "Get" in line:
-                    gets_array.append(line)
+                    index = get_index("Add", "Get", line) #TODO: handle #defines!
+                    if index != -1:
+                        gets_array.append(create_final_name(line, prefix, index))
                 elif "Clear" in line or "Set" in line:
-                    sets_array.append(line)
+                    index = get_index("Clear", "Set", line) #TODO: handle defines!
+                    if index != -1:
+                        sets_array.append(create_final_name(line, prefix, index))
+                elif test_other_cases(line) == 1:
                     print line
-                else:
-                    util_array.append(line)
+                    util_array.append(create_final_name(line))
         
 
-            
+# this is absolutely disgusting.
+def test_other_cases(line):
+    if "#define" not in line and "#ifndef" not in line and "#include" not in line and "#endif" not in line and line.startswith(b'//', 0, 2) == False and line.startswith(b'/*', 0, 2) == False:
+        return 1
+    else:
+        return -1
 
 
+def get_index(first, second, the_string):
+    index = the_string.find(first)
+    if index != -1:
+        return index
+    else:
+        return the_string.find(second)
+
+
+def create_final_name(func_name, lib_prefix="", index=0):
+    return func_name[:index] + lib_prefix + func_name[index:]
 
 #def write_cs_native():
 
@@ -33,8 +53,22 @@ def parse_bo_lib():
 #def write_cpp_source():
     
 
-#def write_cpp_header():
+def write_cpp_header(out_index, ext_index, contents):
+    header = os.getcwd() + "/output/" + outputs[out_index] + extensions[ext_index] 
+    print header
+    with open(header, 'w') as header_file:
+        header_file.write("#define DllExport __declspec(dllexport)")
+        header_file.write("\n\n")
+        for s in contents:
+            header_file.write("extern \"C\" DllExport\t " + s)
+    
 
 
+def main():
+    parse_bo_lib()
+    write_cpp_header(0, 1, gets_array)
+    write_cpp_header(1, 1, sets_array)
+    write_cpp_header(2, 1, util_array)
 
-parse_bo_lib()
+if __name__ == "__main__":
+    main()
